151f34a4650d8d072bed2c8ece5e910e46bf2cfc
Morwenn <morwenn29@hotmail.fr>
2020-02-29 18:24:32 +0100

    Enable additional warnings

--- cmake/cpp-sort-utils.cmake
+++ cmake/cpp-sort-utils.cmake
@@ -3,7 +3,9 @@ macro(cppsort_add_warnings target)
     target_compile_options(${target} PRIVATE
         -Wall -Wextra -Wcast-align -Winline -Wmissing-declarations -Wmissing-include-dirs
         -Wnon-virtual-dtor -Wodr -Wpedantic -Wredundant-decls -Wundef -Wunreachable-code
+        -Wdouble-promotion
         $<$<CXX_COMPILER_ID:GNU>:-Wlogical-op -Wuseless-cast -Wzero-as-null-pointer-constant>
+        $<$<CXX_COMPILER_ID:GNU>:-Wduplicated-cond -Wduplicated-branches>
         # The warning when initializing an std::array is just too much of a bother
         $<$<CXX_COMPILER_ID:Clang>:-Wno-missing-braces>
     )
