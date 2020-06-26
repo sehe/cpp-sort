ba25588cb0f6999a0937cee41a99e4ab6f1da3d3
Morwenn <morwenn29@hotmail.fr>
2020-06-26 23:32:12 +0200

    Don't compile with -Wzero-as-null-pointer-constant
    I somehow get hundreds of what looks like false positives when comparing
    pointers (not even constants), which makes the compiler output passably
    unreadable.

--- cmake/cpp-sort-utils.cmake
+++ cmake/cpp-sort-utils.cmake
@@ -4,7 +4,7 @@ macro(cppsort_add_warnings target)
         -Wall -Wextra -Wcast-align -Winline -Wmissing-declarations -Wmissing-include-dirs
         -Wnon-virtual-dtor -Wodr -Wpedantic -Wredundant-decls -Wundef -Wunreachable-code
         -Wdouble-promotion
-        $<$<CXX_COMPILER_ID:GNU>:-Wlogical-op -Wuseless-cast -Wzero-as-null-pointer-constant>
+        $<$<CXX_COMPILER_ID:GNU>:-Wlogical-op -Wuseless-cast>
         $<$<CXX_COMPILER_ID:GNU>:-Wduplicated-cond -Wduplicated-branches>
     )
 endmacro()
