2092c4f383533983466b67637768dc0b768d093d
Morwenn <morwenn29@hotmail.fr>
2020-07-17 01:12:37 +0200

    Upgrade adapter_storage with [[no_unique_address]]

--- include/cpp-sort/utility/adapter_storage.h
+++ include/cpp-sort/utility/adapter_storage.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2018-2019 Morwenn
+ * Copyright (c) 2018-2020 Morwenn
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -75,7 +75,7 @@ namespace utility
     template<typename Sorter>
     struct adapter_storage<Sorter, false>
     {
-        Sorter sorter;
+        [[no_unique_address]] Sorter sorter;
 
         adapter_storage() = default;
 
